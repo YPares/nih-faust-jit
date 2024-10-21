2ad6a32f14d67670f0f582551a8d4b2aeba7d333
Ywen <yves.pares@gmail.com>
2024-10-21 23:27:16 +0200

    wip

--- faust_jit/src/fundsp_impl.rs
+++ faust_jit/src/fundsp_impl.rs
@@ -1,32 +1,32 @@
 use fundsp::hacker::*;
 use crate::*;
 
-// impl AudioUnit for SingletonDsp {
-//     fn tick(&mut self, input: &[f32], output: &mut [f32]) {
-//         todo!()
-//     }
+impl AudioUnit for SingletonDsp {
+    fn tick(&mut self, input: &[f32], output: &mut [f32]) {
+        todo!()
+    }
 
-//     fn process(&mut self, size: usize, input: &BufferRef, output: &mut BufferMut) {
-//         todo!()
-//     }
+    fn process(&mut self, size: usize, input: &BufferRef, output: &mut BufferMut) {
+        todo!()
+    }
 
-//     fn inputs(&self) -> usize {
-//         todo!()
-//     }
+    fn inputs(&self) -> usize {
+        self.info.num_inputs as usize
+    }
 
-//     fn outputs(&self) -> usize {
-//         todo!()
-//     }
+    fn outputs(&self) -> usize {
+        self.info.num_outputs as usize
+    }
 
-//     fn route(&mut self, input: &SignalFrame, frequency: f64) -> SignalFrame {
-//         todo!()
-//     }
+    fn route(&mut self, _input: &SignalFrame, _frequency: f64) -> SignalFrame {
+        SignalFrame::new(self.outputs())
+    }
 
-//     fn get_id(&self) -> u64 {
-//         todo!()
-//     }
+    fn get_id(&self) -> u64 {
+        todo!()
+    }
 
-//     fn footprint(&self) -> usize {
-//         todo!()
-//     }
-// }
+    fn footprint(&self) -> usize {
+        0
+    }
+}
